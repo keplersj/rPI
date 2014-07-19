d28ed096aae980a61faa2d7f27a09669a367506f
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-07-18 19:16:05 -0600

    Basic something.

---
+++ rpi.rb
@@ -0,0 +1,16 @@
+###
+#
+# This is an idea I've had since middle school.
+# Nothing more, nothing less.
+# And it has a shitty name.
+#
+###
+
+begin
+
+  puts 'This is rPI.'
+  puts "This is an idea I've had since middle school"
+  puts 'Nothing more, nothing less.'
+  puts 'And it has a shitty name.'
+
+end
