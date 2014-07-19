2780eeff23d41aa9351b2fb4bc5ad46e872c2b87
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-07-18 19:43:06 -0600

    Things.

--- rpi.rb
+++ rpi.rb
@@ -13,4 +13,27 @@ begin
   puts 'Nothing more, nothing less.'
   puts 'And it has a shitty name.'
 
+  USE_STATIC_PI = true
+  @pi_file = nil
+  @pi_array = Array.new
+
+  case USE_STATIC_PI
+    when true
+      @pi_file = File.open 'pi.txt' do |file|
+        puts "Using static pi file 'pi.txt' : #{file}."
+        file.each_line do |line|
+          line.to_s.each_char do |char|
+            unless char == '.'
+              @pi_array << char
+            end
+          end
+        end
+      end
+    when false
+    else
+      puts 'Please define the USE_STATIC_PI global variable'
+  end
+
+  puts "The array containing the digits of Pi contains #{@pi_array.count} digits."
+
 end
